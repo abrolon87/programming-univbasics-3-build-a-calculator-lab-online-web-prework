19bccc4340e49a25bf2d22d2ce4edeec61f569db
Amanda R <abrolon87@gmail.com>
2019-11-08 05:43:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,15 +11,16 @@ end
 def division(num1, num2)
   result = num1 / num2
   result
-
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  result 
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num2
+  result
 end
 
 def square_root(num)
